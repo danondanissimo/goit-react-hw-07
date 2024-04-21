ed6c64ef472d8dc8bcef480517f066c7cc4706f8
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-04-21 15:10:36 +0100

    Update SearchBox.jsx

--- src/components/SearchBox/SearchBox.jsx
+++ src/components/SearchBox/SearchBox.jsx
@@ -1,6 +1,15 @@
+import { useDispatch, useSelector } from "react-redux";
 import css from "./SearchBox.module.css";
+import { filterContacts, selectFilter } from "../../redux/filtersSlice";
 
-const SearchBox = ({ filter, onChange }) => {
+const SearchBox = () => {
+  const dispatch = useDispatch();
+  const filter = useSelector(selectFilter);
+  const onChangefilter = (event) => {
+    const action = filterContacts(event.target.value);
+    dispatch(action);
+  };
+
   return (
     <div className={css.container}>
       <label className={css.label}>
@@ -8,7 +17,7 @@ const SearchBox = ({ filter, onChange }) => {
         <input
           type="text"
           value={filter}
-          onChange={onChange}
+          onChange={onChangefilter}
           className={css.field}
         />
       </label>
